85f6ba9ee61c223920e2c3d8b9a48b768569569b
mohad12211 <mohammedkaabi64@gmail.com>
2024-01-11 10:50:55 +0300

    basic pause system

--- src/main.c
+++ src/main.c
@@ -33,40 +33,44 @@ int main(void) {
   Piece fallingPiece = PieceGetRandom();
 
   while (!WindowShouldClose()) {
-    UpdateMusicStream(music);
-    if (time >= GRAVITY_TIME) {
-      bool reachedGround = PieceMoveDown(&fallingPiece);
-      if (reachedGround) {
-        for (int i = 0; i < 4; i++) {
-          const PieceConfiguration *blocks = &fallingPiece.tetromino->rotations[fallingPiece.rotationIndex];
-          Vector2 blockPosition = Vector2Add(blocks->points[i], fallingPiece.position);
-          gameState.board[(int)blockPosition.y][(int)blockPosition.x] = (Block){fallingPiece.tetromino->color, true};
+    if (!gameState.isPaused) {
+      UpdateMusicStream(music);
+      if (time >= GRAVITY_TIME) {
+        bool reachedGround = PieceMoveDown(&fallingPiece);
+        if (reachedGround) {
+          for (int i = 0; i < 4; i++) {
+            const PieceConfiguration *blocks = &fallingPiece.tetromino->rotations[fallingPiece.rotationIndex];
+            Vector2 blockPosition = Vector2Add(blocks->points[i], fallingPiece.position);
+            gameState.board[(int)blockPosition.y][(int)blockPosition.x] = (Block){fallingPiece.tetromino->color, true};
+          }
+          fallingPiece = PieceGetRandom();
         }
-        fallingPiece = PieceGetRandom();
+        time = 0;
       }
-      time = 0;
+      // TODO: controls feel funky
+      if (IsKeyPressedRepeat(KEY_X) || IsKeyPressed(KEY_X)) {
+        PieceRotateClockwise(&fallingPiece);
+      }
+      if (IsKeyPressedRepeat(KEY_Z) || IsKeyPressed(KEY_Z)) {
+        PieceRotateCounterClockwise(&fallingPiece);
+      }
+      if (IsKeyPressedRepeat(KEY_LEFT) || IsKeyPressed(KEY_LEFT)) {
+        PieceMoveLeft(&fallingPiece);
+      }
+      if (IsKeyPressedRepeat(KEY_RIGHT) || IsKeyPressed(KEY_RIGHT)) {
+        PieceMoveRight(&fallingPiece);
+      }
+      if (IsKeyPressedRepeat(KEY_DOWN) || IsKeyPressed(KEY_DOWN)) {
+        time = GRAVITY_TIME;
+      }
+    }
+
+    if (IsKeyPressed(KEY_SPACE)) {
+      gameState.isPaused = !gameState.isPaused;
     }
 
     BeginDrawing();
     ClearBackground(BLACK);
-
-    // TODO: controls feel funky
-    if (IsKeyPressedRepeat(KEY_X) || IsKeyPressed(KEY_X)) {
-      PieceRotateClockwise(&fallingPiece);
-    }
-    if (IsKeyPressedRepeat(KEY_Z) || IsKeyPressed(KEY_Z)) {
-      PieceRotateCounterClockwise(&fallingPiece);
-    }
-    if (IsKeyPressedRepeat(KEY_LEFT) || IsKeyPressed(KEY_LEFT)) {
-      PieceMoveLeft(&fallingPiece);
-    }
-    if (IsKeyPressedRepeat(KEY_RIGHT) || IsKeyPressed(KEY_RIGHT)) {
-      PieceMoveRight(&fallingPiece);
-    }
-    if (IsKeyPressedRepeat(KEY_DOWN) || IsKeyPressed(KEY_DOWN)) {
-      time = GRAVITY_TIME;
-    }
-
     Rectangle playfield = {(WIDTH - BLOCK_LEN * COLUMNS) / 2.0f, HEIGHT / 20.0f, BLOCK_LEN * COLUMNS, BLOCK_LEN * ROWS};
     // playfield without the buffer area
     Rectangle shownPlayfield = {playfield.x, playfield.y + BUFFER_AREA, playfield.width, playfield.height - BUFFER_AREA};
@@ -75,8 +79,8 @@ int main(void) {
     BoardDraw((Vector2){playfield.x, playfield.y});
     DrawRectangleLinesEx(shownPlayfield, 2, GRAY);
     EndScissorMode();
-
     EndDrawing();
+
     time += GetFrameTime();
   }
 
