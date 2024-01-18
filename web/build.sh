e44cb8b342f226a6cb1346205f13ba5b2c9d5c14
mohad12211 <mohammedkaabi64@gmail.com>
2024-01-18 20:05:36 +0300

    Add resources/Sound_1.ogg to web/build.sh

--- web/build.sh
+++ web/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-emcc -o web/index.html src/*.c -Os -Wall -std=c99 -D_DEFAULT_SOURCE -Iweb/ web/libraylib.a  -s USE_GLFW=3 -s EXPORTED_RUNTIME_METHODS=ccall -sGL_ENABLE_GET_PROC_ADDRESS -DPLATFORM_WEB --shell-file web/minshell.html --preload-file resources/Music_1.ogg --preload-file resources/Music_2.ogg --preload-file resources/Music_3.ogg
+emcc -o web/index.html src/*.c -Os -Wall -std=c99 -D_DEFAULT_SOURCE -Iweb/ web/libraylib.a  -s USE_GLFW=3 -s EXPORTED_RUNTIME_METHODS=ccall -sGL_ENABLE_GET_PROC_ADDRESS -DPLATFORM_WEB --shell-file web/minshell.html --preload-file resources/Music_1.ogg --preload-file resources/Music_2.ogg --preload-file resources/Music_3.ogg --preload-file resources/Sound_1.ogg
 git checkout gh-pages
 cp web/index* .
 git commit -am "Update"
